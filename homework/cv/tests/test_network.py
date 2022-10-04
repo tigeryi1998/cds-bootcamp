3c3ce4329d70c8b98d764fe9a479e6b6ef916fb8
Sicheng Yi <sy2199@nyu.edu>
2022-10-03 20:54:36 -0400

    test

--- homework/cv/tests/test_network.py
+++ homework/cv/tests/test_network.py
@@ -1,2 +1,24 @@
 import pytest
 import torch
+from zmq import device
+import bootcamp
+
+@pytest.mark.parametrize('device', ['cpu', pytest.param('cuda', marks=pytest.mark.skipif(not torch.cuda.is_available(), reason='Cuda required' ))])
+
+config = bootcamp.model.PlacesTrainingConfig()
+config.model.width_multiplier = 0.25
+
+model = bootcamp.model.PlacesModel(config)
+model = model.to(device)
+
+dm = bootcamp.dataset.PlacesDataModule(16, '/places365', 1)
+dm.setup()
+
+batch = next( iter(dm.train_dataloader()) )
+torch.save(batch, 'testdata/batch.pt')
+
+batch = torch.load('testdata/batch.pt')
+batch = [ v.to(device)  for v in batch ]
+
+
+
